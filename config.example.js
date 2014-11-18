69d3868afca1da62f0efdb27b9e6f1a7d2ea0ca1
Nathan Vander Wilt <nate@calftrail.com>
2014-11-17 22:54:22 -0800

    hook in twilio

---
+++ config.example.js
@@ -0,0 +1,8 @@
+module.exports = {
+  TWILIO_SID: "{{ACCOUNT SID}}",
+  TWILIO_TOK: "{{AUTH TOKEN}}",
+  SRC_NUMBER: "+15551231234",
+  SMS_DEST: "+15554324321",
+  VOX_DEST: "+15554324321",
+  VOX_URL: "http://twimlets.com/message?Message%5B0%5D=http%3A%2F%2Fwww.televisiontunes.com%2Fdownload.php%3Ff%3DAdam-12&"
+};
--- watcher.node.js
+++ watcher.node.js
@@ -1,14 +1,25 @@
-var LineStream = require('linefeed'),
+var config = require("./config.js"),
     NORMAL_TIMEOUT = 10*60e1,
     ABUSED_TIMEOUT = 45e2,
-    INFO_INTERVAL = 30e2;
+    INFO_INTERVAL = 30e2,
+    lastReceived = "TESTING";
+
+var LineStream = require('linefeed'),
+    fermata = require('fermata');
+fermata.registerPlugin('twilio', function (transport, account_sid, auth_token) {
+  this.base = "https://"+account_sid+":"+auth_token+"@api.twilio.com/2010-04-01/Accounts/"+account_sid;
+  return transport.using('statusCheck').using('autoConvert', "application/x-www-form-urlencoded");
+});
+var twilAcct = fermata.twilio(config.TWILIO_SID, config.TWILIO_TOK);
 
 var watchdog,
-    mood = 'puppy';
+    mood = 'puppy',
+    _req = null;
 function _logMood() {
     console.log("%s | mood: %s.", new Date().toISOString(), mood)
 }
 function _restartWatchdog(snoozeTime) {
+  if (_req) req.abort();
   clearTimeout(watchdog);
   watchdog = setTimeout(howl, snoozeTime);
 }
@@ -17,9 +28,23 @@ function howl() {
     console.warn("Ruhroh.");
     mood = 'unhappy';
     _restartWatchdog(NORMAL_TIMEOUT);
+    _req = twilAcct('Messages').post({
+      From: config.SRC_NUMBER,
+      To: config.SMS_DEST,
+      Body: "Trouble with power status. Last received: "+lastReceived
+    }, handleTwilioResponse);
   } else {
     console.error("SUCH TROUBLE!!1!");
+    _req = twilAcct('Calls').post({
+      From: config.SRC_NUMBER,
+      To: config.VOX_DEST,
+      Url: config.VOX_URL
+    }, handleTwilioResponse);
   }
+  function handleTwilioResponse(e) {
+    _req = null;
+    if (e) throw e;
+  } 
 }
 
 function feedFido() {
@@ -39,6 +64,7 @@ function dropFido() {
 
 process.stdin.pipe(new LineStream({newline:"", objectMode:true})).on('data', function (line) {
     if (!/Received broadcast/.test(line)) return;
+    lastReceived = line;
     var fields = line.split(" "),
         nc = +fields[9].split('=')[1];
     if (nc === 0) feedFido();
